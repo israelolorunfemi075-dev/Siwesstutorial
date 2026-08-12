38f8e9744f86b4d7d6bad514683928c84b19589f
olorunfemiisrael291-spec <olorunfemiisrael291@gmail.com>
2026-08-12 14:02:50 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<head>
+<title>PHP Tutorial</title>
+
+<body>
+
+<?php //echo "Hello World" ;  
+//$num = 3.20;
+//$cast = (int)  $num;
+//echo  $cast;
+//$y = 2;
+//$ans = $y **2;
+//echo $ans;
+
+$x = 99;
+$y = 7;
+$ans = $x%$y;
+echo $ans;
+?>
+
+</body>
+
+
+</head>
+
+
+
